250eb71cd9cc9c34d00b140a279eca2b7bf599bb
kurbiself <kurbiself@mail.ru>
2025-01-12 19:08:50 +0700

    Add H№2 from module 19

--- task1/models.py
+++ task1/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 class Buyer(models.Model):
     name = models.CharField(max_length=30)
-    balance = models.DecimalField(decimal_places=10, max_digits=11)
+    balance = models.DecimalField(decimal_places=2, max_digits=10)
     age = models.IntegerField()
 
 
 class Game(models.Model):
     title = models.CharField(max_length=50)
-    cost = models.DecimalField(decimal_places=10, max_digits=11)
-    size = models.DecimalField(decimal_places=5, max_digits=6)
+    cost = models.DecimalField(decimal_places=2, max_digits=10)
+    size = models.DecimalField(decimal_places=2, max_digits=10)
     description = models.TextField()
     age_limited = models.BooleanField(default=False)
     buyer = models.ManyToManyField(Buyer, related_name='games')
